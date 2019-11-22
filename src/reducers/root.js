181f90238c4a742aad47046e18199e3b7a51d1ba
Minh Duc Vo <minhducsun2002@gmail.com>
2019-11-22 13:00:56 +0700

    Documenting root state tree

--- src/reducers/root.js
+++ src/reducers/root.js
@@ -6,12 +6,14 @@ import { Prism as PrismLanguage } from '../lib/code-view/index';
 
 const initialState = {
     user: {
+        // user information
         loggedIn: false,
         username: null,
         id: null,
         isAdmin: false
     },
     contest: {
+        // contest information
         name : 'defaultContestName',
         time : {
             start: new Date(),
@@ -19,20 +21,30 @@ const initialState = {
         },
         problems: [],
         ext: [],
+        // list of allowed languages, in source file extensions
+        // example: ['.CPP']
         mode: 'OI'
     },
     presets: {
+        // overrideable configurations, like language mappings
+
         language,
+        // human-friendly names mapped from source file extensions
         editor : {
+        // modes mapping for editor components to support syntax highlighting
+        // (and possibly other features)
             monaco: MonacoLanguage
+            // language modes mapping for Monaco
         },
         codeView : {
+        // modes mapping for rendering components to support syntax highlighting
             "react-syntax-highlighter" : {
+            // language modes mapping for `react-syntax-highlighter`
                 prism : PrismLanguage
+                // mapping for Prism.js version
             }
         }
     }
-    // overrideable language
 }
 
 export { initialState };
