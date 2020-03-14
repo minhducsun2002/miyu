0dfe381429c686bf66849473e785a63bb2107a2a
Minh Duc Vo <minhducsun2002@gmail.com>
2020-03-14 12:31:09 +0700

    Adding code to enable theming

--- src/applets/submissions/details/code.js
+++ src/applets/submissions/details/code.js
@@ -1,22 +1,32 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-// TODO : customizable theme
 import * as styles from 'react-syntax-highlighter/dist/esm/styles/prism';
 import Select from 'react-select';
 
 const languagePresetName = "react-syntax-highlighter"
 
 const CodeViewer = ({ code, prismLang, prismTheme, ext = '' }) => {
-    const [theme, setTheme] = useState("vs")
+    const [theme, setTheme] = useState("okaidia")
+    const themeToRecord = s => ({ 
+        value: s,
+        label: <span className="code-text">{s}</span>
+    });
+    const themeMap = prismTheme.map(themeToRecord);
     return (
-        <SyntaxHighlighter
-            style={styles[theme]}
-            showLineNumbers
-            // fallback case when no match
-            language={prismLang[ext] || 'text'}>
-            {code}
-        </SyntaxHighlighter>
+        <>
+            <Select
+                options={themeMap}
+                onChange={({ value }) => setTheme(value)}
+                value={themeToRecord(theme)}/>
+            <SyntaxHighlighter
+                style={styles[theme]}
+                showLineNumbers
+                // fallback case when no match
+                language={prismLang[ext] || 'text'}>
+                {code}
+            </SyntaxHighlighter>
+        </>
     )
 }
 
