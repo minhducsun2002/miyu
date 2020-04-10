9fc71f84b0c99dd732ef25c721514587000eb9fd
Minh Duc Vo <minhducsun2002@gmail.com>
2020-04-10 19:21:15 +0700

    Remove leftover of TypeScript config

--- config/modules.js
+++ config/modules.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const path = require('path');
 const paths = require('./paths');
 const chalk = require('react-dev-utils/chalk');
-const resolve = require('resolve');
 
 /**
  * Get the baseUrl of a compilerOptions object.
@@ -48,29 +47,9 @@ function getAdditionalModulePaths(options = {}) {
 }
 
 function getModules() {
-  // Check if TypeScript is setup
-  const hasTsConfig = fs.existsSync(paths.appTsConfig);
-  const hasJsConfig = fs.existsSync(paths.appJsConfig);
-
-  if (hasTsConfig && hasJsConfig) {
-    throw new Error(
-      'You have both a tsconfig.json and a jsconfig.json. If you are using TypeScript please remove your jsconfig.json file.'
-    );
-  }
-
   let config;
 
-  // If there's a tsconfig.json we assume it's a
-  // TypeScript project and set up the config
-  // based on tsconfig.json
-  if (hasTsConfig) {
-    const ts = require(resolve.sync('typescript', {
-      basedir: paths.appNodeModules,
-    }));
-    config = ts.readConfigFile(paths.appTsConfig, ts.sys.readFile).config;
-    // Otherwise we'll check if there is jsconfig.json
-    // for non TS projects.
-  } else if (hasJsConfig) {
+  if (fs.existsSync(paths.appJsConfig)) {
     config = require(paths.appJsConfig);
   }
 
@@ -79,10 +58,7 @@ function getModules() {
 
   const additionalModulePaths = getAdditionalModulePaths(options);
 
-  return {
-    additionalModulePaths: additionalModulePaths,
-    hasTsConfig,
-  };
+  return { additionalModulePaths: additionalModulePaths };
 }
 
 module.exports = getModules();
--- config/webpack.config.js
+++ config/webpack.config.js
@@ -282,8 +282,7 @@ module.exports = function(webpackEnv) {
       // `web` extension prefixes have been added for better support
       // for React Native Web.
       extensions: paths.moduleFileExtensions
-        .map(ext => `.${ext}`)
-        .filter(ext => useTypeScript || !ext.includes('ts')),
+        .map(ext => `.${ext}`),
       alias: {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
