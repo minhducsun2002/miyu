d967600194a2879cd82ca6cb351100a6ea3cc697
Minh Duc Vo <minhducsun2002@gmail.com>
2020-04-11 10:08:17 +0700

    Load components lazily

---
+++ src/loading.js
@@ -0,0 +1,6 @@
+// fallback component when loading
+
+import React from 'react';
+import { Spinner } from '@blueprintjs/core'
+
+export default () => <div style={{ width: '100%', alignContent: 'center', marginTop: 5 }}><Spinner /></div>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 // importing components
 import Shell from './applets/shell/';
 import Home from './applets/home/';
-import Scoreboard from './applets/scoreboard/';
-import Submissions from './applets/submissions/';
-import Editor from './applets/editor/';
 import Error from './applets/error';
+import Loading from './loading';
+// import components lazily
+const wrapErr = (C) => (..._) => <Error><C {..._}/></Error>
+const wrapSuspense = (C, fallback = <Loading />) => (..._) => <Suspense fallback={fallback}><C {..._}/></Suspense>
+
+const Scoreboard = wrapSuspense(React.lazy(() => import('./applets/scoreboard')));
+const Submissions = wrapSuspense(React.lazy(() => import('./applets/submissions/')));
+const Editor = wrapSuspense(React.lazy(() => import('./applets/editor/')));
 
 // importing routes
 import HomeRoute from './applets/home/route';
@@ -15,7 +20,7 @@ import ScoreboardRoute from './applets/scoreboard/route';
 import SubmissionsRoute from './applets/submissions/route';
 import EditorRoute from './applets/editor/route';
 
-const wrapErr = (C) => (..._) => <Error><C {..._}/></Error>
+
 
 export default () => (
     <Router>
